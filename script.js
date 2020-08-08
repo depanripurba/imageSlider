8cd5a2202aa61742796ced15c6195b8df0109493
depanri purba <depanri.purba@gmail.com>
2020-08-08 07:50:04 +0700

    menambahkan fitur automatic sliding

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 const img = document.querySelector(".gambar")
 const Prev = document.querySelector(".right")
 const Next = document.querySelector(".left")
-let angka = 1 
+let angka = 1  
 
 Prev.onclick = function(){
+    clearInterval(slide)
+    // batas
+    slide = setInterval(()=>{
+        if(angka >= 3){
+            angka = 1 
+        }else{
+            angka++
+        }
+        img.src = "img/"+angka+".jpg"
+        console.log(angka)
+        console.log("ini adalah halaman yang saya bangun dengan javascript")
+        let cek = img.classList.length
+        if(cek >= 2 ){
+            if(img.classList[1]=== "tes"){
+                img.classList.remove("tes")
+                img.classList.add("baru")
+                console.log("sekarang kelas yang aktif adalah kelas tes")
+            }else if(img.classList[1]=== "baru"){
+                img.classList.remove("baru")
+                img.classList.add("tes")
+                console.log("Sekarang kelas yang aktif adalah kelas baru")  
+            }
+        }else{
+        img.classList.add("tes")
+        }
+    },3000)
+    // batas
     let cek = img.classList.length
     if(cek >= 2 ){
         if(img.classList[1]=== "tes"){ 
@@ -28,6 +55,33 @@ Prev.onclick = function(){
     console.log(angka)
 }
 Next.onclick = function(){
+    clearInterval(slide)
+    // batas
+    slide = setInterval(()=>{
+        if(angka >= 3){
+            angka = 1 
+        }else{
+            angka++
+        }
+        img.src = "img/"+angka+".jpg"
+        console.log(angka)
+        console.log("ini adalah halaman yang saya bangun dengan javascript")
+        let cek = img.classList.length
+        if(cek >= 2 ){
+            if(img.classList[1]=== "tes"){
+                img.classList.remove("tes")
+                img.classList.add("baru")
+                console.log("sekarang kelas yang aktif adalah kelas tes")
+            }else if(img.classList[1]=== "baru"){
+                img.classList.remove("baru")
+                img.classList.add("tes")
+                console.log("Sekarang kelas yang aktif adalah kelas baru")  
+            }
+        }else{
+        img.classList.add("tes")
+        }
+    },3000)
+    // batas
     let cek = img.classList.length
     if(cek >= 2 ){
         if(img.classList[1]=== "tes"){
@@ -51,3 +105,31 @@ Next.onclick = function(){
     img.src = "img/"+angka+".jpg"
     console.log(angka)
 }
+
+let slide = setInterval(()=>{
+    if(angka >= 3){
+        angka = 1 
+    }else{
+        angka++
+    }
+    img.src = "img/"+angka+".jpg"
+    console.log(angka)
+    console.log("ini adalah halaman yang saya bangun dengan javascript")
+    let cek = img.classList.length
+    if(cek >= 2 ){
+        if(img.classList[1]=== "tes"){
+            img.classList.remove("tes")
+	        img.classList.add("baru")
+            console.log("sekarang kelas yang aktif adalah kelas tes")
+        }else if(img.classList[1]=== "baru"){
+	        img.classList.remove("baru")
+	        img.classList.add("tes")
+            console.log("Sekarang kelas yang aktif adalah kelas baru")  
+        }
+    }else{
+    img.classList.add("tes")
+    }
+},3000)
+
+img.src = "img/"+angka+".jpg"
+
